37f7241319d458034eefbc647bcfde041e1de895
amylenkyi <32453457+amylenkyi@users.noreply.github.com>
2019-10-09 00:09:06 +0300

    updated connection params for DB in config.py

--- app/web/main/config.py
+++ app/web/main/config.py
@@ -5,10 +5,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     PORT = os.getenv('APP_PORT', '80')
     HOST = os.getenv('APP_HOST', '0.0.0.0')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=os.getenv('POSTGRES_USER', 'postgres'),
-                                                                           pw=os.getenv('POSTGRES_PW', 'postgres'),
-                                                                           url=os.getenv('POSTGRES_URL', 'localhost:5432'),
-                                                                           db=os.getenv('POSTGRES_DB', ''))
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=os.getenv('POSTGRES_USER', 'andriim'),
+                                                                           pw=os.getenv('POSTGRES_PW', 'password'),
+                                                                           url=os.getenv('POSTGRES_URL', '10.188.176.3:5432'),
+                                                                           db=os.getenv('POSTGRES_DB', 'lab4db'))
     DEBUG = False
 
 class DevelopmentConfig(Config):
